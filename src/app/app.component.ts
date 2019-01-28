d6fda96201d41843eb88b6b5897c9665e029d674
Anaissimpz <anaissimpenzwe@gmail.com>
2019-01-28 13:02:00 +0200

    add directive strikethrough

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import {Quote} from './quote'
 })
 export class AppComponent {
   quotes= [
-    new Quote(1,'Create a quote'),
-    new Quote(2,'Upvote a quote'),
-    new Quote(3, 'Downvote the quote'),
-    new Quote(4, 'Delete the quote'),
-    new Quote(5, 'See the number of upvotes and downvotes'),
+    new Quote(1,'Create a quote', 'you will have to create a new quote'),
+    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively'),
+    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote'),
+    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created'),
+    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote'),
     
 ]
 constructor() { }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,17 +7,24 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes= [
-    new Quote(1,'Create a quote'),
-    new Quote(2,'Upvote a quote'),
-    new Quote(3, 'Downvote the quote'),
-    new Quote(4, 'Delete the quote'),
-    new Quote(5, 'See the number of upvotes and downvotes'),
+    new Quote(1,'Create a quote', 'you will have to create a new quote'),
+    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively'),
+    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote'),
+    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created'),
+    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote'),
     
 ]
-
-  constructor() { }
-
+toogleDetails(index){
+  this.quotes[index].showDescription= !this.quotes[index].showDescription;
+}
+completeQuote(isComplete,index){
+  if (isComplete){
+      this.quotes.splice(index,1);
+      }
+  // constructor() { }
+    }
   ngOnInit() {
   }
 
 }
+
