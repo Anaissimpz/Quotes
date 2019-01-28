6dd03830bb527754295bb9c2464c640e929fed76
Anaissimpz <anaissimpenzwe@gmail.com>
2019-01-28 15:53:49 +0200

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import {Quote} from './quote'
 })
 export class AppComponent {
   quotes= [
-    new Quote(1,'Create a quote', 'you will have to create a new quote'),
-    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively'),
-    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote'),
-    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created'),
-    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote'),
+    new Quote(1,'Create a quote', 'you will have to create a new quote', new Date(2019,1,1)),
+    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively' ,new Date(2019,1,1)),
+    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote', new Date(2019,1,1)),
+    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created', new Date(2019,1,1)),
+    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote', new Date(2019,1,1)),
     
 ]
 constructor() { }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,21 +7,28 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes= [
-    new Quote(1,'Create a quote', 'you will have to create a new quote'),
-    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively'),
-    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote'),
-    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created'),
-    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote'),
+    new Quote(1,'Create a quote', 'you will have to create a new quote', new Date(2019,1,1)),
+    new Quote(2,'Upvote a quote', 'users should be able to vote a quote positively' ,new Date(2019,1,1)),
+    new Quote(3, 'Downvote the quote', 'users should be able to vote a quote negatively if they like the quote', new Date(2019,1,1)),
+    new Quote(4, 'Delete the quote', 'one should be able to delete a quote created', new Date(2019,1,1)),
+    new Quote(5, 'See the number of upvotes and downvotes', 'also one should be able to se the votes of the quote', new Date(2019,1,1)),
     
 ]
+deleteQuote(isComplete,index){
+  if (isComplete){
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      
+      if(toDelete){
+          this.quotes.splice(index,1)
+      }}}
+
 toogleDetails(index){
   this.quotes[index].showDescription= !this.quotes[index].showDescription;
 }
-completeQuote(isComplete,index){
+completeQuote(isComplete, index){
   if (isComplete){
-      this.quotes.splice(index,1);
+      this.quotes.splice(index, 1);
       }
-  // constructor() { }
     }
   ngOnInit() {
   }
